cf7673dd196e211370e20eccc287bb7cf4efd485
Karam <karam.taher56@gmail.com>
2026-05-30 16:05:03 +0300

    change email SMTP for Koyeb service

--- backend/mailtrap/nodemailer.js
+++ backend/mailtrap/nodemailer.js
@@ -3,26 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const {
-  SMTP_HOST = "smtp.gmail.com",
-  SMTP_PORT = "465",
-  SMTP_USER,
-  SMTP_PASS,
-  EMAIL_FROM,
-} = process.env;
+const { SMTP_USER, SMTP_PASS, EMAIL_FROM } = process.env;
 
 if (!SMTP_USER || !SMTP_PASS) {
   throw new Error("Missing SMTP_USER/SMTP_PASS in .env");
 }
 
 export const transporter = nodemailer.createTransport({
-  host: SMTP_HOST,
-  port: Number(SMTP_PORT),
-  secure: Number(SMTP_PORT) === 465,
-  auth: { user: SMTP_USER, pass: SMTP_PASS },
-  connectionTimeout: 15000,
-  greetingTimeout: 15000,
-  socketTimeout: 20000,
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: SMTP_USER,
+    pass: SMTP_PASS,
+  },
 });
 
 export const sender = {
