dbbfc52047562d92fefa30f18764412d32b574f7
Karam <karam.taher56@gmail.com>
2026-05-30 14:05:03 +0300

    Fix Dio client conflict

--- frontend/lib/core/network/dio_client.dart
+++ frontend/lib/core/network/dio_client.dart
@@ -12,10 +12,6 @@ class DioClient {
 // 10.10.10.2
   // static const String baseUrl = 'http://192.168.100.9:5000';
   static const String baseUrl = 'https://servifyapp.onrender.com';
-<<<<<<< HEAD
-
-=======
->>>>>>> finishV1
 
   static Future<void> init() async {
     dio = Dio(
